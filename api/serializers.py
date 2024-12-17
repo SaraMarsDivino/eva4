86e3a55bb38e79ee089ba188d4297fcf4bfbee35
ROCHA007 gameplaysdecalidad <rochaulin2@gmail.com>
2024-12-16 23:57:21 -0300

    Version final SIN CSS

--- api/serializers.py
+++ api/serializers.py
@@ -6,31 +6,27 @@ from .models import Clase, Estudiante, Profesor
 class ClaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clase
-        fields = ['id', 'nombre', 'horario', 'descripcion']  # Listar explícitamente los campos
+        fields = ['id', 'nombre', 'horario', 'descripcion']
 
-
-# Serializador para el modelo Estudiante
 class EstudianteSerializer(serializers.ModelSerializer):
-    clases_inscritas = serializers.SerializerMethodField()
+    clases_inscritas = ClaseSerializer(many=True, read_only=True)
+    clases_inscritas_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Clase.objects.all(), many=True, write_only=True, source='clases_inscritas'
+    )
 
     class Meta:
         model = Estudiante
-        fields = ['id', 'nombre', 'correo', 'clases_inscritas']
-
-    def get_clases_inscritas(self, obj):
-        # Devuelve los nombres de las clases inscritas
-        return [clase.nombre for clase in obj.clases_inscritas.all()]
+        fields = ['id', 'nombre', 'correo', 'clases_inscritas', 'clases_inscritas_ids']
 
     def update(self, instance, validated_data):
-        clases_ids = self.initial_data.get('clases_inscritas', [])
-        instance.nombre = validated_data.get('nombre', instance.nombre)
-        instance.correo = validated_data.get('correo', instance.correo)
+        clases_ids = validated_data.pop('clases_inscritas', None)
+        instance = super().update(instance, validated_data)
 
-        if clases_ids:
-            instance.clases_inscritas.set(Clase.objects.filter(id__in=clases_ids))
+        if clases_ids is not None:
+            instance.clases_inscritas.set(clases_ids)  # Sobrescribe las clases inscritas
 
-        instance.save()
         return instance
+
 
 
 # Serializador para el modelo Profesor
